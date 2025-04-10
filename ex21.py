f6a0191b7ea05ce2630a4fd92b1d072017bf7d76
Salvador <73190798+MoraesJoseThiago@users.noreply.github.com>
2025-04-10 09:27:32 -0300

    Update ex21.py

--- ex21.py
+++ ex21.py
@@ -1,1 +1,18 @@
 #21- Faça um jogo de adivinhação onde o programa escolhe um número de 1 a 10 e o usuário tenta adivinhar.
+
+import random
+
+aleatorio = random.randint(1, 10)
+
+chute = 0
+
+while aleatorio != chute:
+    chute = int(input("Seu palpite (entre 1 a 10): "))
+
+    if chute < aleatorio:
+        print("Errou")
+    elif chute > aleatorio:
+        print("ERROU")
+    else:
+        print("Parabéns! Você acertou!")
+        break
