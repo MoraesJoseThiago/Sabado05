9f6201592f4b0d2c36286fed89d33e69a9fd0edd
Salvador <73190798+MoraesJoseThiago@users.noreply.github.com>
2025-04-10 09:22:10 -0300

    Update ex18.py

--- ex18.py
+++ ex18.py
@@ -1,1 +1,15 @@
 #18- Escreva uma função que retorne se um número é primo.
+def eh_primo(n):
+    if n < 2:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+numero = int(input("Digite um número: "))
+
+if eh_primo(numero):
+    print("É primo.")
+else:
+    print("Não é primo.")
