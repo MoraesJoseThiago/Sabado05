e8cbee1c898ae5c2fae41a462568f76b47bf1f0b
Salvador <73190798+MoraesJoseThiago@users.noreply.github.com>
2025-04-10 09:27:41 -0300

    Update ex20.py

--- ex20.py
+++ ex20.py
@@ -1,1 +1,12 @@
 #20- Peça ao usuário para digitar nomes e idades, armazene num dicionário e depois mostre as pessoas maiores de idade.
+
+pessoas = {}
+
+quantidade = int(input("Quantas pessoas você vai cadastrar? "))
+
+for i in range(quantidade):
+    nome = input("Digite o nome: ")
+    idade = int(input("Digite a idade: "))
+    pessoas[nome] = idade
+
+#??
