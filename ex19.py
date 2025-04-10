427333de44a898d55c930a1878257146117de1a1
Salvador <73190798+MoraesJoseThiago@users.noreply.github.com>
2025-04-10 09:23:19 -0300

    Update ex19.py

--- ex19.py
+++ ex19.py
@@ -1,1 +1,9 @@
 #19- Crie uma função que receba uma lista e retorne a soma dos elementos.
+
+def somar_lista(lista):
+    return sum(lista)
+
+numeros = input("Digite os numeros separados por espaço: ")
+lista = list(map(int, numeros.split()))
+
+print("Soma:", somar_lista(lista))
